562c9ba511af8785b6e74fdc0dfe819ee963c396
karan1206 <64606159+karanjoshi1206@users.noreply.github.com>
2020-12-07 20:53:14 +0530

    upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+bar = document.getElementById("bar").addEventListener("click", () => {
+    nav = document.getElementById("nav_items").classList.toggle("active");
+    hid = document.getElementById("hid").classList.toggle("active");
+
+})
+
+// var typed = new Typed(".typing", {
+//     strings: ["Web Developer", "Student", "Coder", "Learner"],
+//     typeSpeed: 100,
+//     backSpeed: 60,
+//     loop: true,
+// });
